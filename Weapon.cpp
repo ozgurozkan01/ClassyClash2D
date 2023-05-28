c09e894b0e047f85f89277714c435abd68e551c2
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2023-05-28 04:04:20 +0300

    add weapon class

---
+++ Weapon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ozgur on 5/28/2023.
+//
+
+#include "Weapon.h"
---
+++ Weapon.h
@@ -0,0 +1,14 @@
+//
+// Created by ozgur on 5/28/2023.
+//
+
+#ifndef CLASSYCLASH2D_WEAPON_H
+#define CLASSYCLASH2D_WEAPON_H
+
+
+class Weapon {
+
+};
+
+
+#endif //CLASSYCLASH2D_WEAPON_H
